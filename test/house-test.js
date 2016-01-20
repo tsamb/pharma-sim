8023d381fab87dfd733ce3f70503edd9113615a9
Sam Blackman <sblackman@gmail.com>
2016-01-21 10:51:39 +1300

    set up test harness with mocha and should using npm

---
+++ test/house-test.js
@@ -0,0 +1,13 @@
+var requirejs = require("requirejs");
+var should = require("should");
+
+requirejs.config({
+  baseUrl: 'scripts',
+  paths: {
+    models: 'models',
+    presenters: '../test/mocks/presenters'
+  }
+});
+
+describe('House', function() {
+});
