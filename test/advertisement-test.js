5bd79d4aab2679f4d462ce051e088d41ca127413
Sam Blackman <sblackman@gmail.com>
2016-08-02 23:21:43 +0100

    test Advertisement model

---
+++ test/advertisement-test.js
@@ -0,0 +1,38 @@
+var requirejs = require("requirejs");
+var should = require("should");
+
+requirejs.config({
+  baseUrl: 'scripts',
+  paths: {
+    models: 'models',
+    presenters: '../test/mocks/presenters',
+    errors: '../test/mocks/errors'
+  }
+});
+
+describe('Advertisement', function() {
+  var Advertisement, ad;
+  before(function(done) {
+    requirejs(['models/advertisement'], function(AdvertisementConstructor) {
+      Advertisement = AdvertisementConstructor;
+      done();
+    });
+  });
+
+  beforeEach(function() {
+    ad = new Advertisement({name: "Test Ad", hype: 500, price: 1000});
+  })
+
+  describe('#instantiation', function(){
+    it('should create new instances of Advertisement', function(){
+      ad.should.have.property('name');
+      ad.should.have.property('hype');
+      ad.should.have.property('price');
+      ad.should.have.property('id');
+    });
+
+    it('should have an id starting with "advertisement-"', function() {
+      ad.id.should.match(/advertisement-.*/);
+    });
+  });
+});
