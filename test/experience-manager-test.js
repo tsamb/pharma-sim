c6a87e49c27cdf113a74929073c689198ae2ace4
Sam Blackman <sblackman@gmail.com>
2016-08-02 07:10:48 +0300

    write tests for ExperienceManager#level and template all other methods

--- test/experience-manager-test.js
+++ test/experience-manager-test.js
@@ -36,4 +36,69 @@ describe('ExperienceManager', function() {
     it('should start at level 1', function() {
       em.level().should.eql(1);
     });
+
+    it('should be level 1 until 1000 xp', function() {
+      em.experience = 500;
+      em.level().should.eql(1);
+      em.experience = 1000;
+      em.level().should.eql(1);
+    });
+
+    it('should be level 2 between 1001 and 8000 xp', function() {
+      em.experience = 1001;
+      em.level().should.eql(2);
+      em.experience = 8000;
+      em.level().should.eql(2);
+    });
+
+    it('should be level 3 between 8001 and 27000 xp', function() {
+      em.experience = 8001;
+      em.level().should.eql(3);
+      em.experience = 27000;
+      em.level().should.eql(3);
+    });
+
+    it('should be level 4 between 27001 and 64000 xp', function() {
+      em.experience = 27001;
+      em.level().should.eql(4);
+      em.experience = 64000;
+      em.level().should.eql(4);
+    });
+
+    it('should be level 5 between 64001 and 125000 xp', function() {
+      em.experience = 64001;
+      em.level().should.eql(5);
+      em.experience = 125000;
+      em.level().should.eql(5);
+    });
+  });
+
+  describe('#currentLevelReachedAt', function() {
+
+  });
+
+  describe('#nextLevelReachedAt', function() {
+
+  });
+
+  describe('#experienceGainedThisLevel', function() {
+
+  });
+
+  describe('#experienceRemainingThisLevel', function() {
+
+  });
+
+  describe('#experienceBetweenCurrentLevels', function() {
+
+  });
+
+  describe('#percentageOfCurrentLevelComplete', function() {
+
+  });
+
+  describe('#increase', function() {
+
+  });
+
 });
