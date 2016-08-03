a2958f5c460df0c37abfa4068762150a06e18706
Sam Blackman <sblackman@gmail.com>
2016-08-03 21:35:02 +0100

    test Neighborhood#findHouse and #updateHype

--- test/neighborhood-test.js
+++ test/neighborhood-test.js
@@ -22,7 +22,7 @@ describe('Neighborhood', function() {
   });
 
   beforeEach(function() {
-    emtpyHood = new Neighborhood;
+    emptyHood = new Neighborhood;
     fullHood = new Neighborhood;
     for (var i = 0; i < 5; i++) {
       fullHood.houses.push(new House({budget: 80, frequency: 5, active: true, hypeToActivate: 0}));
@@ -31,15 +31,15 @@ describe('Neighborhood', function() {
 
   describe('#instantiation', function(){
     it('should create new instances of Neighborhood', function(){
-      emtpyHood.should.have.property('houses', []);
+      emptyHood.should.have.property('houses', []);
     });
   });
 
   describe('#addHouse', function(){
     it('adds House objects to the neighborhood', function(){
-      emtpyHood.addHouse({budget: 100, frequency: 10, active: true, hypeToActivate: 0}).should.match(/house-.*/);
-      emtpyHood.houses.should.have.length(1);
-      emtpyHood.houses[0].should.be.an.instanceOf(House);
+      emptyHood.addHouse({budget: 100, frequency: 10, active: true, hypeToActivate: 0}).should.match(/house-.*/);
+      emptyHood.houses.should.have.length(1);
+      emptyHood.houses[0].should.be.an.instanceOf(House);
     });
   });
 
@@ -47,6 +47,24 @@ describe('Neighborhood', function() {
     it('calls #updateReadiness on every house', function() {
       var spies = fullHood.houses.map(function(house) { return sinon.spy(house, "updateReadiness") });
       fullHood.updateHouseReadiness();
+      spies.forEach(function(spy) {
+        spy.called.should.eql(true);
+      });
+    });
+  });
+
+  describe('#findHouse', function() {
+    it('returns the house in the collection that matches the ID provided', function() {
+      var houseToFind = {id: "house-15"};
+      emptyHood.houses = [{id: "house-10"}, houseToFind, {id: "house-20"}];
+      emptyHood.findHouse("house-15").should.equal(houseToFind);
+    });
+  });
+
+  describe('#updateHype', function() {
+    it('calls #updateHype on every house', function() {
+      var spies = fullHood.houses.map(function(house) { return sinon.spy(house, "updateHype") });
+      fullHood.updateHype();
       spies.forEach(function(spy) {
         spy.called.should.eql(true);
       });
