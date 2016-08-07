25bcaaf5d587c04e3c7df7787597f680e465d51d
Sam Blackman <sblackman@gmail.com>
2016-08-07 19:18:50 +0300

    move data for houses, offers and ads into constants

--- scripts/controller.js
+++ scripts/controller.js
@@ -18,40 +18,48 @@ function(ResourceManager, House, SupplyOffer, Days, Neighborhood, MarketingManag
 
   // <<<<<<<< INITIAL SET UP >>>>>>>>
 
+  var HOUSE_ARGS = [
+    {budget: 80, frequency: 5, active: true, hypeToActivate: 0},
+    {budget: 120, frequency: 20, active: true, hypeToActivate: 0},
+    {budget: 150, frequency: 23, active: true, hypeToActivate: 0},
+    {budget: 250, frequency: 30, active: true, hypeToActivate: 0},
+    {budget: 150, frequency: 25, active: false, hypeToActivate: 2},
+    {budget: 180, frequency: 27, active: false, hypeToActivate: 2},
+    {budget: 200, frequency: 18, active: false, hypeToActivate: 3},
+    {budget: 300, frequency: 60, active: false, hypeToActivate: 4},
+    {budget: 140, frequency: 16, active: false, hypeToActivate: 4},
+    {budget: 100, frequency: 5, active: false, hypeToActivate: 4},
+    {budget: 140, frequency: 22, active: false, hypeToActivate: 5},
+    {budget: 120, frequency: 15, active: false, hypeToActivate: 5},
+    {budget: 180, frequency: 12, active: false, hypeToActivate: 6},
+    {budget: 320, frequency: 40, active: false, hypeToActivate: 6},
+    {budget: 240, frequency: 10, active: false, hypeToActivate: 7}
+  ];
+
+  var OFFER_ARGS = [
+    {amount: 10, price: 1000},
+    {amount: 100, price: 7500},
+    {amount: 500, price: 30000},
+    {amount: 1000, price: 50000}
+  ];
+
+  var AD_ARGS = [
+    {name: "Paper Boy", price: 500, hype: 1500},
+    {name: "Flyer Drop", price: 2000, hype: 8000},
+    {name: "Flyer Handout", price: 4000, hype: 20000},
+    {name: "Referral Program", price: 10000, hype: 60000},
+    {name: "Social Media", price: 30000, hype: 210000},
+    {name: "Web Banner Ads", price: 50000, hype: 400000},
+    {name: "Billboard", price: 100000, hype: 1000000},
+    {name: "TV Spot", price: 500000, hype: 6000000},
+    {name: "Superbowl Ad", price: 3000000, hype: 45000000}
+  ];
+
   Controller.prototype.init = function() {
-    this.addHouse({budget: 80, frequency: 5, active: true, hypeToActivate: 0});
-    this.addHouse({budget: 120, frequency: 20, active: true, hypeToActivate: 0});
-    this.addHouse({budget: 150, frequency: 23, active: true, hypeToActivate: 0});
-    this.addHouse({budget: 250, frequency: 30, active: true, hypeToActivate: 0});
-    this.addHouse({budget: 150, frequency: 25, active: false, hypeToActivate: 2});
-    this.addHouse({budget: 180, frequency: 27, active: false, hypeToActivate: 2});
-    this.addHouse({budget: 200, frequency: 18, active: false, hypeToActivate: 3});
-    this.addHouse({budget: 300, frequency: 60, active: false, hypeToActivate: 4});
-    this.addHouse({budget: 140, frequency: 16, active: false, hypeToActivate: 4});
-    this.addHouse({budget: 100, frequency: 5, active: false, hypeToActivate: 4});
-    this.addHouse({budget: 140, frequency: 22, active: false, hypeToActivate: 5});
-    this.addHouse({budget: 120, frequency: 15, active: false, hypeToActivate: 5});
-    this.addHouse({budget: 180, frequency: 12, active: false, hypeToActivate: 6});
-    this.addHouse({budget: 320, frequency: 40, active: false, hypeToActivate: 6});
-    this.addHouse({budget: 240, frequency: 10, active: false, hypeToActivate: 7});
-
-    this.addSupplyOffer({amount: 10, price: 1000});
-    this.addSupplyOffer({amount: 100, price: 7500});
-    this.addSupplyOffer({amount: 500, price: 30000});
-    this.addSupplyOffer({amount: 1000, price: 50000});
-
-    this.addAdvertisement({name: "Paper Boy", price: 500, hype: 1500});
-    this.addAdvertisement({name: "Flyer Drop", price: 2000, hype: 8000});
-    this.addAdvertisement({name: "Flyer Handout", price: 4000, hype: 20000});
-    this.addAdvertisement({name: "Referral Program", price: 10000, hype: 60000});
-    this.addAdvertisement({name: "Social Media", price: 30000, hype: 210000});
-    this.addAdvertisement({name: "Web Banner Ads", price: 50000, hype: 400000});
-    this.addAdvertisement({name: "Billboard", price: 100000, hype: 1000000});
-    this.addAdvertisement({name: "TV Spot", price: 500000, hype: 6000000});
-    this.addAdvertisement({name: "Superbowl Ad", price: 3000000, hype: 45000000});
-
+    HOUSE_ARGS.forEach(function(args) { this.addHouse(args) }.bind(this));
+    OFFER_ARGS.forEach(function(args) { this.addSupplyOffer(args) }.bind(this));
+    AD_ARGS.forEach(function(args) { this.addAdvertisement(args) }.bind(this));
     this.addPropertyUpgrade();
-
     this.addPurchasableSwitches();
   }
 
